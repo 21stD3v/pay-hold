f89e5bdc55b33190e0b54f076bfba39cc6b782eb
abduljalal-s <nightmadecoder@gmail.com>
2026-03-12 15:09:27 +0100

    nextcofig-fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+	turbopack: {
+		root: __dirname,
+	},
 	// Optimize for Nigerian bandwidth
 	images: {
 		formats: ["image/webp"],
