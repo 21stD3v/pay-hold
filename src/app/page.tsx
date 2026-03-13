cdf127ad5926b16e46c023c3011d5160c1f47a39
abduljalal-s <nightmadecoder@gmail.com>
2026-03-13 11:37:51 +0100

    links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -654,7 +654,7 @@ export default function HomePage() {
 							</p>
 							<div className='flex gap-4'>
 								<a
-									href='#'
+									href='https://x.com/casualcravehq'
 									className='w-10 h-10 bg-gray-800 hover:bg-[#0d4d7d] rounded-lg flex items-center justify-center transition-colors'
 								>
 									<svg
@@ -666,8 +666,8 @@ export default function HomePage() {
 									</svg>
 								</a>
 								<a
-									href='#'
-									className='w-10 h-10 bg-gray-800 hover:bg-[#1a7a4a] rounded-lg flex items-center justify-center transition-colors'
+									href='https://www.instagram.com/casualcrave/'
+									className='w-10 h-10 bg-gray-800 hover:bg-[#7d0f0079] rounded-lg flex items-center justify-center transition-colors'
 								>
 									<svg
 										className='w-5 h-5'
@@ -675,18 +675,6 @@ export default function HomePage() {
 										viewBox='0 0 24 24'
 									>
 										<path d='M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z' />
-									</svg>
-								</a>
-								<a
-									href='#'
-									className='w-10 h-10 bg-gray-800 hover:bg-[#0d4d7d] rounded-lg flex items-center justify-center transition-colors'
-								>
-									<svg
-										className='w-5 h-5'
-										fill='currentColor'
-										viewBox='0 0 24 24'
-									>
-										<path d='M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.324v-21.35c0-.732-.593-1.325-1.325-1.325z' />
 									</svg>
 								</a>
 							</div>
@@ -737,7 +725,7 @@ export default function HomePage() {
 							<ul className='space-y-3'>
 								<li>
 									<Link
-										href='/terms'
+										href='/privacy-policy'
 										className='text-gray-400 hover:text-white transition-colors hover:translate-x-1 inline-block'
 									>
 										Terms & Conditions
@@ -745,7 +733,7 @@ export default function HomePage() {
 								</li>
 								<li>
 									<Link
-										href='/privacy'
+										href='/privacy-policy'
 										className='text-gray-400 hover:text-white transition-colors hover:translate-x-1 inline-block'
 									>
 										Privacy Policy
@@ -753,7 +741,7 @@ export default function HomePage() {
 								</li>
 								<li>
 									<Link
-										href='/refund-policy'
+										href='/privacy-policy'
 										className='text-gray-400 hover:text-white transition-colors hover:translate-x-1 inline-block'
 									>
 										Refund Policy
@@ -761,7 +749,7 @@ export default function HomePage() {
 								</li>
 								<li>
 									<Link
-										href='/dispute-resolution'
+										href='/privacy-policy'
 										className='text-gray-400 hover:text-white transition-colors hover:translate-x-1 inline-block'
 									>
 										Dispute Resolution
@@ -790,7 +778,7 @@ export default function HomePage() {
 											d='M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
 										/>
 									</svg>
-									<span>support@payhold.ng</span>
+									<span>support@ payholdit@gmail.com</span>
 								</li>
 								<li className='flex items-start gap-2'>
 									<svg
@@ -840,26 +828,6 @@ export default function HomePage() {
 							<p className='text-gray-400 text-sm'>
 								&copy; 2026 PayHold. All rights reserved.
 							</p>
-							<div className='flex gap-6 text-sm text-gray-400'>
-								<Link
-									href='/sitemap'
-									className='hover:text-white transition-colors'
-								>
-									Sitemap
-								</Link>
-								<Link
-									href='/security'
-									className='hover:text-white transition-colors'
-								>
-									Security
-								</Link>
-								<Link
-									href='/faq'
-									className='hover:text-white transition-colors'
-								>
-									FAQ
-								</Link>
-							</div>
 						</div>
 					</div>
 				</div>
