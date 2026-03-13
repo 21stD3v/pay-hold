7ec8889064170b6f6faaa456d8d0737abb95d1d8
abduljalal-s <nightmadecoder@gmail.com>
2026-03-13 11:23:08 +0100

    updated-working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
-};
+  // remove output: 'export' — NextAuth requires server runtime
+  images: {
+    domains: ['lh3.googleusercontent.com'], // for Google profile pictures
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
